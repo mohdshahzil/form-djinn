fca2c357db423a784ebd80b35460b9cc76919e65
mohdshahzil <theshahzil@gmail.com>
2024-02-23 23:50:00 +0530

    database-connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import { Button } from "@/components/ui/button";
 import FormGenerator from "./form-generator";
 import Header from "@/components/ui/header";
 import { SessionProvider } from "next-auth/react";
-const page = () => {
+import { db } from "@/db";
+import { forms } from "@/db/schema";
+export default async function page() {
+  const forms = await db.query.forms.findMany();
+  console.log(forms);
   return (
     <>
       <SessionProvider>
@@ -14,6 +18,4 @@ const page = () => {
       </SessionProvider>
     </>
   );
-};
-
-export default page;
+}
